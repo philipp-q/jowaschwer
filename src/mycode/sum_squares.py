cd0088f16b25cf0d2301430041d41cda0f08b960
Philipp Schleich <schleich.philipp@rwth-aachen.de>
2020-05-16 11:10:49 +0200

    update stuff

--- src/mycode/sum_squares.py
+++ src/mycode/sum_squares.py
@@ -6,7 +6,6 @@
 def main(dictio):
     out = 0.0
     for key in dictio.keys():
-        for kkeyy in dictio[key].keys():
-            out += dictio[key][kkeyy]**2
+        out += dictio[key]**2
 
     return {'value': out}
